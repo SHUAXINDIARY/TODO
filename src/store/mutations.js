3524f61179c11be2b9734ecf33f0c332853365c3
shuaxin <986384495@qq.com>
2019-10-07 21:09:12 +0800

    mutations代码优化

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -2,11 +2,12 @@ import Vue from 'vue'
 let vm = new Vue();
 export default {
     addItem(state, payload) {
-        // 添加新项目
+        // 获取传递过来的数据
         let item = {
             status: false,
             note: payload.item
         };
+        // 添加新项目
         vm.$set(state[payload.data].items, state[payload.data].items.length, item);
         // 添加完毕后执行回调
         payload.succ();
